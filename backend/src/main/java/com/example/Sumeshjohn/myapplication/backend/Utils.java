7614df4b7efa505ace3b97ff4af527b1684d761e
Boomesh <sumesh@bnotions.com>
2016-01-18 11:32:58 -0500

    Update Utils.java
    Good grief, why was this method even added

--- backend/src/main/java/com/example/Sumeshjohn/myapplication/backend/Utils.java
+++ backend/src/main/java/com/example/Sumeshjohn/myapplication/backend/Utils.java
@@ -5,6 +5,7 @@ package com.example.Sumeshjohn.myapplication.backend;
  */
 public class Utils {
     //helper method to check empty string
+    // If you're reading this and you have any pride, use `TextUtils.isEmpty()`
     static boolean isStringEmpty(String s) {
         return s == null || s.isEmpty();
     }
